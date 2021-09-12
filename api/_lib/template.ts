d8eaba78de4d29f7e1a0368ece99db087d95169f
yokinist <ikd618@gmail.com>
2021-09-12 14:27:18 +0900

    update style

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,111 +1,150 @@
-
-import { readFileSync } from 'fs';
-import marked from 'marked';
-import { sanitizeHtml } from './sanitizer';
-import { ParsedRequest } from './types';
-const twemoji = require('twemoji');
-const twOptions = { folder: 'svg', ext: '.svg' };
+import { readFileSync } from "fs";
+import marked from "marked";
+import { sanitizeHtml } from "./sanitizer";
+import { ParsedRequest } from "./types";
+const twemoji = require("twemoji");
+const twOptions = { folder: "svg", ext: ".svg" };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+
+const noto = readFileSync(
+  `${__dirname}/../_fonts/NotoSansCJKjp-Regular.woff2`
+).toString("base64");
+
+const notoBold = readFileSync(
+  `${__dirname}/../_fonts/NotoSansCJKjp-Bold.woff2`
+).toString("base64");
+
+const rglr = readFileSync(
+  `${__dirname}/../_fonts/Inter-Regular.woff2`
+).toString("base64");
+const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
+  "base64"
+);
+const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
+  "base64"
+);
 
 function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
-    let radial = 'lightgray';
+  let background = "white";
+  let foreground = "black";
 
-    if (theme === 'dark') {
-        background = 'black';
-        foreground = 'white';
-        radial = 'dimgray';
+  if (theme === "dark") {
+    background = "#090719";
+    foreground = "white";
+  }
+  return `
+    @font-face {
+        font-family: 'noto-sans-cjk-jp';
+        font-style:  normal;
+        font-weight: normal;
+        src: url(data:font/woff2;charset=utf-8;base64,${noto}) format('woff2');
     }
-    return `
+    @font-face {
+        font-family: 'noto-sans-cjk-jp';
+        font-style:  normal;
+        font-weight: bold;
+        src: url(data:font/woff2;charset=utf-8;base64,${notoBold}) format('woff2');
+    }
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
-
+    @font-face {
+        font-family: 'Inter';
+        font-style:  normal;
+        font-weight: normal;
+        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+    }
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
         font-weight: bold;
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
-
     @font-face {
         font-family: 'Vera';
         font-style: normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
       }
-
     body {
         background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
-        height: 100vh;
+        height: calc(100vh - 90px);
         display: flex;
         text-align: center;
         align-items: center;
         justify-content: center;
+        border: 45px solid #5a67d8;
+        padding: 0;
     }
-
     code {
         color: #D400FF;
         font-family: 'Vera';
         white-space: pre-wrap;
         letter-spacing: -5px;
     }
-
     code:before, code:after {
         content: '\`';
     }
-
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
-    }
-
     .logo {
         margin: 0 75px;
     }
-
     .plus {
         color: #BBB;
         font-family: Times New Roman, Verdana;
         font-size: 100px;
     }
-
     .spacer {
         margin: 150px;
     }
-
     .emoji {
         height: 1em;
         width: 1em;
         margin: 0 .05em 0 .1em;
         vertical-align: -0.1em;
     }
-    
+
+    .wrapper {
+      width: 80%;
+    }
+
     .heading {
-        font-family: 'Inter', sans-serif;
+        font-family: 'noto-sans-cjk-jp', 'Inter', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
+        font-weight: bold;
         font-style: normal;
         color: ${foreground};
         line-height: 1.8;
-    }`;
+        width: 80%;
+        margin: 0 auto;
+    }
+
+    .footer {
+      display: flex;
+      font-family: 'noto-sans-cjk-jp', 'Inter', sans-serif;
+      color: ${foreground};
+      line-height: 1.8;
+      font-size: 64px;
+      width: 80%;
+      margin: 0 auto;
+      justify-content: between;
+    }
+
+    .site {
+      display: inline-block;
+      font-weight: bold;
+      margin: 0 auto;
+    }
+    `;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
-    return `<!DOCTYPE html>
+  const { text, theme, md, fontSize, siteTitle } =
+    parsedReq;
+  return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
@@ -114,33 +153,15 @@ export function getHtml(parsedReq: ParsedRequest) {
         ${getCss(theme, fontSize)}
     </style>
     <body>
-        <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
-            </div>
-            <div class="spacer">
+        <div class="wrapper">
             <div class="heading">${emojify(
-                md ? marked(text) : sanitizeHtml(text)
+              md ? marked(text) : sanitizeHtml(text)
             )}
+            </div>
+            <div class="footer">
+              <p class="site">${siteTitle}</p>
             </div>
         </div>
     </body>
 </html>`;
-}
-
-function getImage(src: string, width ='auto', height = '225') {
-    return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`
-}
-
-function getPlusSign(i: number) {
-    return i === 0 ? '' : '<div class="plus">+</div>';
 }
